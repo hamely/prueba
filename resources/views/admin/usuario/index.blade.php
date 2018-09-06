209fa678166094d0ce130d622ac7027e9852b4a2
hamely <sarmiento101147@gmail.com>
2018-09-06 16:59:51 -0500

    a

--- resources/views/admin/usuario/index.blade.php
+++ resources/views/admin/usuario/index.blade.php
@@ -60,9 +60,7 @@
                             <a href="{{route('usuario.edit',$item->id)}}" class="btn btn-primary btn-xs">
 								Editar
 							</a>		
-							<a href="" class="btn btn-danger btn-xs">
-								Eliminar
-							</a>
+
                             </td>
                             </tr>
                         @endforeach
