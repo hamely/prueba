148ef58271ed181a19e59adabaf194bd3bd4879c
hamely <sarmiento101147@gmail.com>
2018-09-06 23:23:14 -0500

    value en usuario

--- resources/views/admin/usuario/create.blade.php
+++ resources/views/admin/usuario/create.blade.php
@@ -41,8 +41,8 @@
                         <label class="control-label col-md-3 col-sm-3 col-xs-12" for="name">Nombre de usuario <span class="required">*</span>
                         </label>
                         <div class="col-md-6 col-sm-6 col-xs-12">
-                        {!!Form::text('name',null, ['class' => 'form-control btn-sm' , 'id' => 'nombre','autocomplete' => 'off'])!!}
-                          <!--<input id="nombre"  name="nombre"  class="form-control col-md-7 col-xs-12" data-validate-length-range="6" data-validate-words="2" placeholder="Ingrese nombre del usuario" type="text">-->
+                        <!--{!!Form::text('name',null, ['class' => 'form-control btn-sm' , 'id' => 'nombre','autocomplete' => 'off'])!!}-->
+                          <input id="name"  name="name"  class="form-control col-md-7 col-xs-12" data-validate-length-range="6" data-validate-words="2" placeholder="Ingrese nombre del usuario" type="text">
                         </div>
                       </div>
                       <div class="item form-group">
