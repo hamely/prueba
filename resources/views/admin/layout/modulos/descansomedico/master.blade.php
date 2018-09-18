833eadfa331a57887e5b8e636bfd3a1a952c7014
hamely <sarmiento101147@gmail.com>
2018-09-18 16:21:10 -0500

    redireccion

--- resources/views/admin/layout/modulos/descansomedico/master.blade.php
+++ resources/views/admin/layout/modulos/descansomedico/master.blade.php
@@ -41,7 +41,7 @@
         <div class="col-md-3 left_col">
           <div class="left_col scroll-view">
             <div class="navbar nav_title" style="border: 0;">
-              <a href="" class="site_title"></i> <span>Cyber Management</span></a>
+              <a href="{{'modulos'}}" class="site_title"></i> <span>Cyber Management</span></a>
             </div>
             <div class="clearfix"></div>
             <br/>
